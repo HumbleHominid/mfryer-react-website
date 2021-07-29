cffb3a84024689f9b7a746806d00e0a437267db4
Michael Fryer <michael.d.fryer@gmail.com>
2021-07-29 11:18:46 -0700

    Adding rel=noreferrer to a-tags for offsite links

--- src/footer.js
+++ src/footer.js
@@ -6,10 +6,10 @@ export default function Footer() {
         <dd className="col">michael.d.fryer@gmail.com</dd>
         <div className="w-100"></div>
         <dt className="col text-right">GitHub</dt>
-        <dd className="col"><a target="_blank" href="https://github.com/HumbleHominid">HumbleHominid</a></dd>
+        <dd className="col"><a target="_blank" rel="noreferrer" href="https://github.com/HumbleHominid">HumbleHominid</a></dd>
         <div className="w-100"></div>
         <dt className="col text-right">LinkedIn</dt>
-        <dd className="col"><a target="_blank" href="https://www.linkedin.com/in/michael-fryer-bb017013a/">Michael Fryer</a></dd>
+        <dd className="col"><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/michael-fryer-bb017013a/">Michael Fryer</a></dd>
         <div className="w-100"></div>
         <dt className="col text-right">Resume</dt>
         <div className="col"><a href="#resume" download="Michael_Fryer_Resume.pdf">Download</a></div>
