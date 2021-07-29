82de9e6d440b33b4604f106f23ef419b136d4467
Michael Fryer <michael.d.fryer@gmail.com>
2021-07-28 18:09:20 -0700

    Adding a footer with some off-site links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import CustomNavbar from './nav';
+import Footer from './footer';
 
 function App() {
   return (
     <div className="App">
       <CustomNavbar />
       <h1>My First App!</h1>
+      <Footer />
     </div>
   );
 }
