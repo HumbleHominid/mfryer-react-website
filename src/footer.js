b46f65218b2016de3b9e3edbb9b94c44d2865469
Michael Fryer <michael.d.fryer@gmail.com>
2021-07-28 18:09:46 -0700

    Adding lost footer file

---
+++ src/footer.js
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer id="application-footer" class="mx-auto">
+      <dl class="row">
+        <dt class="col text-right">Email</dt>
+        <dd class="col">michael.d.fryer@gmail.com</dd>
+        <div class="w-100"></div>
+        <dt class="col text-right">GitHub</dt>
+        <dd class="col"><a target="_blank" href="https://github.com/HumbleHominid">HumbleHominid</a></dd>
+        <div class="w-100"></div>
+        <dt class="col text-right">LinkedIn</dt>
+        <dd class="col"><a target="_blank" href="https://www.linkedin.com/in/michael-fryer-bb017013a/">Michael Fryer</a></dd>
+        <div class="w-100"></div>
+        <dt class="col text-right">Resume</dt>
+        <div class="col"><a href="#resume" download="Michael_Fryer_Resume.pdf">Download</a></div>
+      </dl>
+    </footer>
+  );
+}
