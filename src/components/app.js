aef44e941597947766f01318317eba5c71840aaa
Michael Fryer <michael.d.fryer@gmail.com>
2021-07-29 16:14:23 -0700

    fixing the page being horizontally scrollable when it shouldn't be

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,23 @@
 import CustomNavbar from './nav';
 import Footer from './footer';
-import Container from 'react-bootstrap/Container';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './home';
 import About from './about';
 
 function App() {
   return (
-    <Router>
-      <div id="application">
+    <div id="application">
+      <Router>
         <CustomNavbar />
-        <Container id="application-content-container">
+        <article id="application-content-container">
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/about" component={About} />
           </Switch>
-        </Container>
-        <Footer />
-      </div>
-    </Router>
+        </article>
+      </Router>
+      <Footer />
+    </div>
   );
 }
 
