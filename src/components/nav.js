c77b24b7a9c9726d79be5273b17a7b1288fc8fae
Michael Fryer <michael.d.fryer@gmail.com>
2021-07-29 13:21:02 -0700

    Wrap navbar in header

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,29 +5,31 @@ import Container from 'react-bootstrap/Container';
 
 export default function CustomNavbar() {
   return (
-    <Navbar bg="dark" expand="lg" variant="dark" sticky="top">
-      <Container>
-      <LinkContainer to="/">
-        <Navbar.Brand href="/">
-          <img
-            alt=""
-            src="/logo.png"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{' '}
-          Home
-        </Navbar.Brand>
-      </LinkContainer>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
-            <LinkContainer to="/about">
-              <Nav.Link>About</Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header>
+      <Navbar bg="dark" expand="lg" variant="dark" sticky="top">
+        <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src="/logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Home
+          </Navbar.Brand>
+        </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <LinkContainer to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
