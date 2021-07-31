f3d003b7122962023c72e83830d3b70cdfb04d55
Michael Fryer <michael.d.fryer@gmail.com>
2021-07-30 17:35:44 -0700

    removing my email from the footer since it's in the header now

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,6 @@
-import { Email } from '../refLinks';
-
 export default function Footer() {
   return (
     <footer id="application-footer" className="mx-auto">
-      <dl className="row">
-        <dt className="col text-right">Email</dt>
-        <dd className="col">michael.d.fryer@gmail.com</dd>
-      </dl>
     </footer>
   );
 }
