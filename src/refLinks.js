c4fe9c1423b4be6388700c36aa9d90f7a22c750f
Michael Fryer <michael.d.fryer@gmail.com>
2021-08-03 16:25:43 -0700

    fixing an unnecessary rename on export

--- src/refLinks.js
+++ src/refLinks.js
@@ -2,4 +2,4 @@ const Email = 'michael.d.fryer@gmail.com';
 const GitHub = 'https://github.com/HumbleHominid';
 const LinkedIn = 'https://www.linkedin.com/in/michael-fryer-bb017013a/';
 
-export { Email, GitHub as GitHub, LinkedIn };
+export { Email, GitHub, LinkedIn };
