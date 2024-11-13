1c14479765e17b13fadc269d78acbae04c2df815
HumbleHominid <michael.d.fryer@gmail.com>
2024-11-13 14:57:08 -0700

    updating home page

--- src/components/home.js
+++ src/components/home.js
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div>
       <h1 className="display-2">Home</h1>
-      <p className="lead">This is a website I have created to help showcase some of my skills as a front-end developer. The front-end is build with React while there is a small express server set up in the backend. Everything is just stood up in an ec2-instance.</p>
+      <p className="lead">Welcome to my website. This website is built in React and hosted on an AWS Fargate instance.</p>
 
-      <p>Please checkout <LinkContainer to="/about"><button type="button" className="btn btn-link btn-link-de-chonk">About</button></LinkContainer> for more information about me, and <LinkContainer to="/game"><button type="button" className="btn btn-link btn-link-de-chonk">Game</button></LinkContainer> for a space game that I made. And finally, the source for this application is available on my <a target="_blank" rel="noreferrer" href={GitHub}>GitHub's</a> main page.</p>
+      <p>Please checkout <LinkContainer to="/about"><button type="button" className="btn btn-link btn-link-de-chonk">About</button></LinkContainer> for more information about myself. I have also built a game in JavaScript and it is playable over at <LinkContainer to="/game"><button type="button" className="btn btn-link btn-link-de-chonk">Game</button></LinkContainer>. This game is sure to be a hit as it's never been seen before (I joke). And finally, the source for this application is available on <a target="_blank" rel="noreferrer" href={GitHub}>GitHub's</a>.</p>
     </div>
   );
 }
